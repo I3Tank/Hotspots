bf6bd8f51eb1e923189f48e08a1df5ca8a3e68d5
FHAleex <alex.babakhel@stud.fh-campuswien.ac.at>
2021-05-19 18:11:53 +0200

    js experiment

---
+++ info.js
@@ -0,0 +1,20 @@
+// // document.addEventListener("DOMContentLoaded", function(event) {
+//
+//
+//     function openNav() {
+//         document.getElementById("mySidebar").style.width = "12%";
+//         document.getElementById("main").style.marginLeft = "12%";
+//     }
+//
+//     // let staropen = document.getElementById("staropen");
+//     // staropen.addEventListener("click", openNav);
+//     // staropen.onclick = openNav();
+//
+//     function closeNav() {
+//         document.getElementById("mySidebar").style.width = "0";
+//         document.getElementById("main").style.marginLeft = "0";
+//     }
+//
+//     let starclose = document.getElementById("starclose");
+//     staropen.addEventListener("click", closeNav);
+// // });
---
+++ main.js
@@ -0,0 +1,57 @@
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+// function showSlides() {
+//     let slideIndex = 0;
+//     let i;
+//     const slides = document.querySelectorAll("img.p1");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     slides[slideIndex-1].style.display = "block";
+//     return showSlides2();
+//     // return showSlides3();
+//     //ajax request
+// }
+// showSlides();
+//
+//     // </script>
+//     // <script className="slide">
+//
+// function showSlides2() {
+//     let slideIndex2 = 0;
+//     let i;
+//     const slides2 = document.querySelectorAll("img.p2");
+//     for (i = 0; i < slides2.length; i++) {
+//     slides2[i].style.display = "none";
+//     }
+//     slideIndex2++;
+//     if (slideIndex2 > slides2.length) {slideIndex2 = 1}
+//     slides2[slideIndex2-1].style.display = "block";
+//     setTimeout(showSlides, 2000);
+//
+// }
+// showSlides2();
+//
+// // </script>
+//     // <script>
+//
+// function showSlides3() {
+//     let slideIndex3 = 0;
+//     let i;
+//     const slides = document.querySelectorAll("img.p3");
+//     for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//     }
+//     slideIndex3++;
+//     if (slideIndex3 > slides.length) {slideIndex3 = 1}
+//     slides[slideIndex3-1].style.display = "block";
+//     setTimeout(showSlides3, 2000);
+//     }
+// showSlides3();
+//
+// // </script>
+// });
