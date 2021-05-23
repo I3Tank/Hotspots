a7478846b8af10b33cb27d6f2197189d47d07bd5
FHAleex <alex.babakhel@stud.fh-campuswien.ac.at>
2021-05-23 16:32:24 +0200

    js one solution

--- info.js
+++ info.js
@@ -1,20 +1,25 @@
-// // document.addEventListener("DOMContentLoaded", function(event) {
-//
-//
-//     function openNav() {
-//         document.getElementById("mySidebar").style.width = "12%";
-//         document.getElementById("main").style.marginLeft = "12%";
-//     }
-//
-//     // let staropen = document.getElementById("staropen");
-//     // staropen.addEventListener("click", openNav);
-//     // staropen.onclick = openNav();
-//
-//     function closeNav() {
-//         document.getElementById("mySidebar").style.width = "0";
-//         document.getElementById("main").style.marginLeft = "0";
-//     }
-//
-//     let starclose = document.getElementById("starclose");
-//     staropen.addEventListener("click", closeNav);
-// // });
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "12%";
+        document.getElementById("main").style.marginLeft = "12%";
+    }
+
+    // let staropen = document.getElementById("staropen");
+    // let node = document.createTextNode("Star");
+    // staropen.appendChild(node);
+    // let xy = staropen.addEventListener("click", openNav);
+    // staropen.appendChild(xy);
+
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
+
+    // let starclose = document.getElementById("starclose");
+    // let node2 = document.createTextNode("x");
+    // starclose.appendChild(node2);
+    // let xy2 = starclose.addEventListener("click", closeNav);
+    // starclose.appendChild(xy2);
+// });
--- main.js
+++ main.js
@@ -1,22 +1,113 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var slideIndex = 0;
+    function carousel2() {
+        // let slideIndex = 0;
+
+        let i;
+        let x = document.querySelectorAll("img.p2");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+        setTimeout(carousel2, 2000); // Change image every 2 seconds
+    }
+    carousel2();
+    carousel2();
+
+    function carousel() {
+        // let slideIndex = 0;
+
+        let i;
+        let x = document.querySelectorAll("img.p1");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+        setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
+    carousel();
+
+    function showSlides() {
+        let i;
+        let slides = document.querySelectorAll("img.p3");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+            setTimeout(showSlides, 2000); // Change image every 2 seconds
+        // return showSlides2();
+        // return showSlides3();
+        //ajax request
+    }
+    showSlides();
+
+    // function showSlides2() {
+    //     let i;
+    //     let slides2 = document.querySelectorAll("img.p3");
+    //     for (i = 0; i < slides2.length; i++) {
+    //         slides2[i].style.display = "none";
+    //     }
+    //     slideIndex++;
+    //     if (slideIndex > slides2.length) {slideIndex = 1}
+    //     slides2[slideIndex-1].style.display = "block";
+    //     setTimeout(showSlides2, 2000); // Change image every 2 seconds
+    //     // return showSlides2();
+    //     // return showSlides3();
+    //     //ajax request
+    // }
+    // showSlides2();
 
 
 
-// function showSlides() {
-//     let slideIndex = 0;
+
+// function showslides() {
+//     let slideindex = 0;
 //     let i;
 //     const slides = document.querySelectorAll("img.p1");
+//
 //     for (i = 0; i < slides.length; i++) {
+//         // setTimeout(  slides[i].style.display = "none", 2000);
 //         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {slideIndex = 1}
-//     slides[slideIndex-1].style.display = "block";
-//     return showSlides2();
-//     // return showSlides3();
-//     //ajax request
+//     }return showslides2();
 // }
-// showSlides();
+//
+//     function showslides2() {
+//         let slideindex = 0;
+//         let i;
+//         const slides = document.querySelectorAll("img.p2");
+//
+//         for (i = slides.length; i >= 0; i--) {
+//             slides[i].style.display = "none";
+//         }
+//         setTimeout(showslides2, 2000);
+//     }
+//     showslides2();
+
+
+    // for (i = 0; i < slides.length; i++) {
+    //     // slides[i]. = 10;
+    //     // slides[i].style.display = "none";
+    //     setTimeout( slides[i].style.display = "none", 2000);
+    //         slideIndex++;
+    //     if (slideIndex > slides.length) {slideIndex = 1}
+    //     slides[slideIndex-1].style.display = "block";
+    //     i++;
+    //     if (i > 3) {
+    //         i = 0
+    //     }
+    // }
+
+
+
+
+
 //
 //     // </script>
 //     // <script className="slide">
@@ -54,4 +145,4 @@
 // showSlides3();
 //
 // // </script>
-// });
+});
