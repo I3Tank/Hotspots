191fd2590168a6beeaaea1bbe2450b26cde3b241
gucccikev <kevser.akpinar@stud.fh-campuswien.ac.at>
2021-06-25 17:26:19 +0200

    added Generalinformation

--- api/models/HotspotModel.js
+++ api/models/HotspotModel.js
@@ -4,70 +4,104 @@ const hotspots = new Map();
 hotspots.set("0", {
     weather: "Rainy",
     location: "pics\/NewYork\/NYC_Map_EmpireStateBuilding.png",
-    generalInformation: "This is the first General Information from a web server.I hope this works.",
+    generalInformation: "At its top floor, the Empire State Building stands 1,250 feet (380 meters) tall which makes it currently the 4th tallest building in New York City." +
+        "The uses of the Empire State Building are varied, while commonly being used for office spaces, observation decks, broadcasting, it also at times served as a wedding location, filming site." +
+        "The Empire State Building’s antenna is hit by lightning an average of 25 times a year with different colors which represent different festivals or events, making for some incredible photo opportunities.",
     additionalInformation1: "Monday to Thursday 1 - 10pm Friday to Sunday 12pm - 12am",
-    additionalInformation2: "Gibts keine",
+    additionalInformation2: "Of the top five tallest buildings in New York City, the Empire State Building is by far the oldest." +
+        "Construction of the building began in 1930 and, after an incredible 13 months (just 410 days), was completed in 1931.",
 });
 hotspots.set("1", {
     weather: "Rainyy",
     location: "pics\/NewYork\/NYC_Map_StatueOfLiberty.png",
-    generalInformation: "This is the second General Information from a web server.\nI hope this works.",
+    generalInformation: "The statue is formally known as \"Liberty Enlightening the World.\"" +
+        "There are 354 steps to the crown and 25 windows in the crown." +
+        "The seven rays of the statue's crown represent the seven seas and continents of the world." +
+        "The tablet held in the left hand of the statue reads in Roman numerals \"July 4th, 1776.\"[Independence Day] (JULY IV MDCCLXXVI)",
     additionalInformation1: "Daily/Everyday 8:30am - 4pm ",
-    additionalInformation2: "Gibts eine",
+    additionalInformation2: "The Statue of Liberty was a gift to the United States from the people of France to commemorate 100 years of Franco-American friendship." +
+        "The statue also commemorates the centennial of America's independence." +
+        "It was sculpted by Frederic Auguste Bartholdi. The framework was designed by French engineer Gustave Eiffel.",
 });
 hotspots.set("2", {
     weather: "Rainyyy",
     location: "pics\/NewYork\/NYC_Map_TimeSquare.png",
-    generalInformation: "This is the third General Information from a web server.\nI hope this works.",
+    generalInformation: "While One Times Square is pretty much empty, it is the home for a giant wall of over 50 billboards that generates over $20 million in income each year." +
+        "It’s also the home to the famous New Year’s Eve ball that millions of people watch drop to ring in the New Year." +
+        "Times Square is not actually square! It’s made up of two triangles, where Broadway cuts through the grid. " +
+        "You can also see the largest LED display in the world, a 37-foot high sign owned by the NASDAQ.",
     additionalInformation1: "All the time?..",
-    additionalInformation2: "Gibts eine",
+    additionalInformation2: "It once went by the name Longacre Square." +
+        "It took the name Times Square from the New York Times headquarters, which in 1904 moved to One Times Square.",
 });
 
 //----------LONDON----------
 hotspots.set("3", {
     weather: "Sunshine",
     location: "pics\/London\/London_Map_BigBen.png",
-    generalInformation: "This is the first General Information from a web server.\nI hope this works.",
+    generalInformation: "Big Ben chimes on the hour and has quarter bells that chime every fifteen minutes." +
+        "The clock tower is the focus of New Year celebrations in the United Kingdom, with radio and TV stations tuning to its chimes to welcome the start of the year." +
+        "On Remembrance Day, the chimes of Big Ben are broadcast to mark the 11th hour of the 11th day of the 11th month.\n" +
+        "A light in the clock tower tells when the House of Commons is in session.",
     additionalInformation1: "Not open?",
-    additionalInformation2: "Gibts keine",
+    additionalInformation2: "The name Big Ben originally referred to just the bell but now it encompasses the clock, the tower and the bell.",
 });
 hotspots.set("4", {
     weather: "Rain",
     location: "pics\/London\/London_Map_BuckinghamPalace.png",
-    generalInformation: "This is the second General Information from a web server.\nI hope this works.",
+    generalInformation: "Buckingham Palace is the Queen's official and main royal London home. It has been the official London residence of Britain's monarchy since 1837." +
+        "Buckingham Palace is used also for the administrative work for the monarchy. It is here in the state apartments that Her Majesty receives and entertains guests invited to the Palace." +
+        "When the Queen is at home, you can see her royal flag flying from the flag pole on top of Buckingham Palace. This flag is called the Royal Standard.",
     additionalInformation1: "Daily 9:30am - 5:30pm",
-    additionalInformation2: "Gibts eine",
+    additionalInformation2: "Buckingham Palace was originally a grand house built by the Dukes of Buckingham for his wife." +
+        "George IV began transforming it into a palace in 1826." +
+        "Queen Victoria was the first monarch to live there.",
 });
 hotspots.set("5", {
     weather: "Rain",
     location: "pics\/London\/London_Map_LondonEye.png",
-    generalInformation: "This is the third General Information from a web server.\nI hope this works.",
+    generalInformation: " At 443 feet high, the London Eye is currently the fourth-largest Ferris wheel in the world, but it doesn't even crack the top 20 tallest structures in London itself." +
+        "Having 32 capsules, a ride on the London Eye takes 30 minutes, and it travels at a speed of about 0.6 miles per hour." +
+        "With more than 3.5 million people checking out the Eye every year, it's now the most popular paid tourist attraction in the U.K.",
     additionalInformation1: "Daily 11am - 6pm",
-    additionalInformation2: "Gibts eine",
+    additionalInformation2: "Just like the Eiffel Tower, the London Eye was originally planned as a temporary structure; built to stand on Lambeth Council's ground on the banks of the Thames for around five years." +
+        "In July 2002, Lambeth Council granted the Eye a permanent licence.",
 });
 
 //----------ISTANBUL----------
 hotspots.set("6", {
     weather: "Sunshine",
     location: "pics\/Istanbul\/Istanbul_Map_Ayasofya.png",
-    generalInformation: "This is the first General Information from a web server.\nI hope this works.",
+    generalInformation: "Ayasofya a.k.a Hagia Sophia also called Church of the Holy Wisdom or Church of the Divine Wisdom, an important Byzantine structure in Istanbul and one of the world’s great monuments." +
+        "The building reflects the religious changes that have played out in the region over the centuries, with the minarets and inscriptions of Islam as well as the lavish mosaics of Christianity.",
     additionalInformation1: "From 8-25h",
-    additionalInformation2: "Gibts keine",
+    additionalInformation2: "Once the largest cathedral in the world, Ayasofya has stood for more than 1500 years along the banks of the Bosporus Strait and has housed three religious groups." +
+        "It has been destroyed and rebuilt over 8 times.",
 });
 hotspots.set("7", {
     weather: "Rain",
     location: "pics\/Istanbul\/Istanbul_Map_MaidensTower.png",
-    generalInformation: "This is the second General Information from a web server.\nI hope this works.",
-    additionalInformation1: "From 7-26h",
-    additionalInformation2: "Gibts eine",
+    generalInformation: "Today, Maidens Tower, Turkish Kizkulesi, is a very popular and classy restaurant and cafeteria-bar." +
+        "It offers 360 degree views of the Bosphorus and the old city, especially at night." +
+        "There are several shuttle boats going to the tower at certain times from the European side to the Asian side of Istanbul." +
+        "It's also a popular place for summer time weddings.",
+    additionalInformation1: "Daily 9am - 7pm",
+    additionalInformation2: "According to the best-known story, the Byzantine emperor had a daughter." +
+        "The mighty ruler said that his daughter would be bitten by a snake before she turned 18 and would die." +
+        "Then the emperor repaired the structure in the middle of the sea and placed his daughter in the tower." +
+        "However the princess could not escape the fate and was bitten by a snake which came to the island in a fruit basket.",
 });
 hotspots.set("8", {
     weather: "Rain",
     location: "pics\/Istanbul\/Istanbul_Map_Taksim.png",
-    generalInformation: "This is the third General Information from a web server.\nI hope this works.",
+    generalInformation: "Taksim Square is the main square of Istanbul's New Districts." +
+        "It lies on the east end of the busy shopping street, Istiklal Caddesi (Independence Avenue), thus it is one of the termini of the nostalgic train that runs along Independence Avenue." +
+        "Travel agencies, restaurants, cafés and pubs surround the square along with some of Istanbul's most luxurious hotels.",
     additionalInformation1: "All the time?",
-    additionalInformation2: "Gibts eine",
+    additionalInformation2: "The square's name derives from Arabic meaning 'division' or 'distribution." +
+        "The city's main water line, built by Sultan Mahmud I in 1732, ended here and branched off to other parts of the city",
 });
+
 //TODO Insert multiple Hotspots with correct Information
 
 class HotspotModel{
